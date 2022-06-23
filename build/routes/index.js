e607b1b32d84d8a9efc1e0a96566826ba2d20c72
ErdemGhifuri <kesukaan.ultra@gmail.com>
2022-06-23 08:52:38 +0700

    build 2

--- build/routes/index.js
+++ build/routes/index.js
@@ -4,13 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const AuthLayer_1 = require("../helpers/AuthLayer");
 const WebhookRequestHandler_1 = require("../handler/WebhookRequestHandler");
 const route = express_1.default.Router();
 // === CHECK CONNECTION
 route.get("/", (req, res) => res.sendStatus(200));
 // == HANDLER
-route.post("/webhook-request", AuthLayer_1.AuthLayer, WebhookRequestHandler_1.WebhookRequestHandler.webhookRequest);
+route.post("/webhook-request", WebhookRequestHandler_1.WebhookRequestHandler.webhookRequest);
 // === MISC
 route.get("*", (req, res) => res.sendStatus(404));
 route.post("*", (req, res) => res.sendStatus(404));
--- routes/index.ts
+++ routes/index.ts
@@ -8,7 +8,7 @@ const route: Router = express.Router();
 route.get("/", (req: Request, res: Response) => res.sendStatus(200));
 
 // == HANDLER
-route.post("/webhook-request", AuthLayer, WebhookRequestHandler.webhookRequest);
+route.post("/webhook-request", WebhookRequestHandler.webhookRequest);
 
 // === MISC
 route.get("*", (req: Request, res: Response) => res.sendStatus(404));
